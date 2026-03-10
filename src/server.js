ee9b28375058a7104bd2c54898d894491b188ede
BrundaRachuaiah <brundadr315@gmail.com>
2026-03-10 23:34:01 +0530

    added the deployment URL

--- src/server.js
+++ src/server.js
@@ -34,7 +34,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
-  "https://nitro-frontend-sooty.vercel.app"
+  'https://nitro-frontend-sooty.vercel.app'
 ];
 
 const corsOptions = {
